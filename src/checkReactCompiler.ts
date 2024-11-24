c30a55ca561e67566a2b4afdb623883bcdf07184
Blazej Kustra <blazej.kustra@swmansion.com>
2024-11-24 18:07:44 +0100

    Initial version with babel

---
+++ src/checkReactCompiler.ts
@@ -0,0 +1,110 @@
+import { transformFromAstSync } from "@babel/core";
+import * as BabelParser from "@babel/parser";
+import * as path from "path";
+import * as vscode from "vscode";
+
+export type LoggerEvent = {
+  filename: string | null;
+  kind?: string;
+  fnLoc: { start: { line: number; column: number; index: number } };
+};
+
+function runBabelPluginReactCompiler(
+  BabelPluginReactCompiler: any,
+  text: string,
+  file: string,
+  language: "flow" | "typescript"
+) {
+  const successfulCompilations: Array<LoggerEvent> = [];
+  const failedCompilations: Array<LoggerEvent> = [];
+
+  const logger = {
+    logEvent(filename: string | null, rawEvent: LoggerEvent) {
+      const event = { ...rawEvent, filename };
+      switch (event.kind) {
+        case "CompileSuccess": {
+          successfulCompilations.push(event);
+          return;
+        }
+        case "CompileError":
+        case "CompileDiagnostic":
+        case "PipelineError":
+          failedCompilations.push(event);
+          return;
+      }
+    },
+  };
+
+  const COMPILER_OPTIONS = {
+    noEmit: true,
+    compilationMode: "infer",
+    panicThreshold: "critical_errors",
+    environment: {
+      enableTreatRefLikeIdentifiersAsRefs: true,
+    },
+    logger,
+  };
+
+  const ast = BabelParser.parse(text, {
+    sourceFilename: file,
+    plugins: [language, "jsx"],
+    sourceType: "module",
+  });
+  const result = transformFromAstSync(ast, text, {
+    filename: file,
+    highlightCode: false,
+    retainLines: true,
+    plugins: [[BabelPluginReactCompiler, COMPILER_OPTIONS]],
+    sourceType: "module",
+    configFile: false,
+    babelrc: false,
+  });
+  // eslint-disable-next-line eqeqeq
+  if (result?.code == null) {
+    throw new Error(
+      `Expected BabelPluginReactForget to codegen successfully, got: ${result}`
+    );
+  }
+
+  return {
+    successfulCompilations,
+    failedCompilations,
+  };
+}
+
+export function checkReactCompiler(sourceCode: string, filename: string) {
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage(
+      "No workspace folder is open. Please open a project first."
+    );
+    return { successfulCompilations: [], failedCompilations: [] };
+  }
+
+  let BabelPluginReactCompiler: any;
+
+  try {
+    const workspacePath = workspaceFolder.uri.fsPath;
+    const nodeModulesPath = path.join(workspacePath, "node_modules");
+    BabelPluginReactCompiler = require(path.join(
+      nodeModulesPath,
+      "babel-plugin-react-compiler"
+    ));
+  } catch (error: any) {
+    vscode.window.showErrorMessage(
+      `Failed to load react-compiler-healthcheck. Make sure it is installed in the workspace. Error: ${error.message}`
+    );
+    return { successfulCompilations: [], failedCompilations: [] };
+  }
+
+  try {
+    return runBabelPluginReactCompiler(
+      BabelPluginReactCompiler,
+      sourceCode,
+      filename,
+      "typescript"
+    );
+  } catch {
+    return { successfulCompilations: [], failedCompilations: [] };
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { detectFunctionComponents } from "./detectFunctionComponent";
+import { checkReactCompiler, LoggerEvent } from "./checkReactCompiler";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,21 +13,78 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "react-compiler-marker" is now active!'
   );
 
+  const magicSparksDecoration = vscode.window.createTextEditorDecorationType({
+    after: {
+      contentText: " ✨",
+    },
+  });
+
+  const blockIndicatorDecoration = vscode.window.createTextEditorDecorationType(
+    {
+      after: {
+        contentText: " 🚫",
+      },
+    }
+  );
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
     "react-compiler-marker.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from React Compiler Marker1  ✨!"
-      );
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const { successfulCompilations, failedCompilations } =
+          checkReactCompiler(
+            editor.document.getText(),
+            editor.document.fileName
+          );
+
+        await updateDecorations(
+          editor,
+          magicSparksDecoration,
+          successfulCompilations
+        );
+
+        await updateDecorations(
+          editor,
+          blockIndicatorDecoration,
+          failedCompilations
+        );
+      }
+
+      vscode.window.showInformationMessage("Hello World3 ✨!");
     }
   );
 
   context.subscriptions.push(disposable);
+}
+
+async function updateDecorations(
+  editor: vscode.TextEditor,
+  decorationType: vscode.TextEditorDecorationType,
+  logs: LoggerEvent[]
+) {
+  // Create decorations for each detected line
+  const decorations: vscode.DecorationOptions[] = logs.map((log) => {
+    const range = new vscode.Range(
+      log.fnLoc.start.line - 1,
+      8,
+      log.fnLoc.start.line - 1,
+      8
+    );
+    return { range };
+  });
+
+  console.log(`%%% decorations`, JSON.stringify(logs, null, 2));
+
+  editor.setDecorations(decorationType, decorations);
+
+  const fileUri = editor?.document.uri;
+  if (fileUri === undefined) {
+    return;
+  }
 }
 
 // This method is called when your extension is deactivated
