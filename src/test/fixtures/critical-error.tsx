acbd7dfb74399cbfe497619be3d8cd811ce9cdbb
Blazej Kustra <blazej.kustra@swmansion.com>
2025-11-04 22:15:06 +0100

    Remove commented-out code

--- src/test/fixtures/critical-error.tsx
+++ src/test/fixtures/critical-error.tsx
@@ -6,11 +6,7 @@ export default function Foo() {
   async function onSubmit() {
     try {
       await doStuff();
-      // OPTION 1: CRASHING
     } catch (error) {
-      // OPTION 2: NOT CRASHING
-      // } catch (err) {
-      //   const error = err;
       const errorString = (() => {
         if (!(error instanceof Error)) {
           return "An unexpected error occurred";
