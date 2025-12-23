1fd981e3d4f2d17f345365e82b1c1d4253ee23c1
Blazej Kustra <blazej.kustra@swmansion.com>
2025-12-24 00:00:22 +0100

    Update IntelliJ platform configuration and increment self-update date

--- packages/webstorm-plugin/build.gradle.kts
+++ packages/webstorm-plugin/build.gradle.kts
@@ -35,7 +35,9 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea(providers.gradleProperty("platformVersion"))
+        val type = providers.gradleProperty("platformType")
+        val version = providers.gradleProperty("platformVersion")
+        create(type, version)
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
