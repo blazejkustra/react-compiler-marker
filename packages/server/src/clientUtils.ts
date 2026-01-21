f1b1d42260090a3dc0244c6ecef070f9a5cc2fae
simeon.grancharov <simeon.grancharov@cubbystorage.com>
2026-01-21 07:05:07 +0200

    Remove duplicate isNeovimClient fn

--- packages/server/src/clientUtils.ts
+++ packages/server/src/clientUtils.ts
@@ -2,10 +2,6 @@ export function isVSCodeClient(clientName?: string): boolean {
   return clientName?.toLowerCase().includes("visual studio code") ?? false;
 }
 
-export function isIntelliJClient(clientName?: string): boolean {
-  return clientName?.toLowerCase().includes("intellij") ?? false;
-}
-
 export function isNVimClient(clientName?: string): boolean {
   return clientName?.toLowerCase().includes("neovim") ?? false;
 }
@@ -14,14 +10,10 @@ export function supportsCommandLinks(clientName?: string): boolean {
   return !isNVimClient(clientName);
 }
 
-export function isNeovimClient(clientName?: string): boolean {
-  return clientName?.toLowerCase().includes("neovim") ?? false;
-}
-
 export function shouldEnableHover(clientName?: string): boolean {
   return isNVimClient(clientName);
 }
 
 export function shouldEnableDiagnostics(clientName?: string): boolean {
-  return isNeovimClient(clientName);
+  return isNVimClient(clientName);
 }
